aec3300d5dde552faadaf663431fcd2f87556c17
JolynWong <shiroyami08@gmail.com>
2022-05-12 11:58:49 +0800

    Setting Up Build System (CMake)
    Trying out setting up of CMake for building project, for easier porting between different working coomputers

---
+++ src/Core/Application.h
@@ -0,0 +1,21 @@
+/*******************************************************************************
+* \file			Application.h
+
+* \brief		Defines necessary functionalities of an application developed
+*				in FireflyEngine.
+*******************************************************************************/
+#pragma once
+
+namespace FireflyEngine
+{
+	// Specifications for defining an application 
+	struct AppSpecifications
+	{
+		const char* const m_
+	};
+
+	class Application final
+	{
+
+	};
+}
---
+++ src/EntryPoint.cpp
@@ -0,0 +1,14 @@
+﻿/*******************************************************************************
+* \file			EntryPoint.cpp
+
+* \brief		First Point of Entry of any application developed using Firefly
+				Engine.
+*******************************************************************************/
+#include <iostream>
+
+int main()
+{
+	// <TODO>: Application class, handles setup and stores impt managers used
+	std::cout << "Setup CMake, print ALL CLEAR message ..." << std::endl;
+	std::cin.get();
+}
